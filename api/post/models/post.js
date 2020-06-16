abfc60ac4cd7fd38a2834db94c95e26936e28e70
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-06-16 09:52:24 +0200

    change to mongodb

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
+const axios = require("axios");
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/models.html#life-cycle-callbacks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterUpdate(data) {
+      console.log("updateHook");
+      const a = {
+        event_type: "hola",
+      };
+      axios
+        .post(
+          "https://api.github.com/repos/kylawamg/devspain-front/dispatches",
+          a
+        )
+        .catch((err) => {
+          console.log(err);
+          // Ignore
+        });
+    },
+  },
+};
